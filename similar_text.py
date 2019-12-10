118002e7adc6a4266ca5ee638345b0ede11eda1f
OrionMat <36226295+OrionMat@users.noreply.github.com>
2019-12-10 17:45:06 +0000

    added subject search

--- similar_text.py
+++ similar_text.py
@@ -12,26 +12,21 @@ def removeStopWords(wordList):
     return cleanedList
 
 def sentenceToPOS(sentence):
-    """ 
-        takes a spaCy sentence (object) and returns:
-        * lemma sentence
-        * words (str list) 
-        * adjectives (str list)
-        * verbs (str list)
+    """ takes a spaCy sentence (object) and returns:
+        * lemma sentence    (str)
+        * words             (str list) 
+        * subjects          (str list)
     """
     lemmaSent = ""
     wordList = []
-    adjList = []
-    verbList = []
+    subjList = []
     for token in sentence:
         lemmaSent = lemmaSent + " " + token.lemma_
         if token.lemma_ not in string.punctuation:
             wordList += [token.lemma_]
-        if token.pos_ == "ADJ":
-            adjList += [token.lemma_]
-        if token.pos_ == "VERB":
-            verbList += [token.lemma_]
-    return lemmaSent, wordList, adjList, verbList
+        if token.dep_ == "nsubj":
+            subjList += [token.lemma_]
+    return lemmaSent, wordList, subjList
 
 def getJaccardSim(A, B): 
     """ computes Jaccard Similarity between word groups A and B """
@@ -57,39 +52,37 @@ docArticle = nlp(article)
 
 # gets articles three most similar sentences to statment (lemmatized -> jaccard score)
 statementWords = [token.lemma_ for token in docStatement if token.lemma_ not in string.punctuation]
-statementAdjs = [token.lemma_ for token in docStatement if token.pos_ == "ADJ"]
-statementVerbs = [token.lemma_ for token in docStatement if token.pos_ == "VERB"]
+statementSubjs = [token.lemma_ for token in docStatement if token.dep_ == "nsubj"]
+print("statement subjs: ", statementSubjs)
 statementNoStops = removeStopWords(statementWords)
 jaccardScoreList = []
 jaccardNoStopsList = []
-jaccardAdjList = []
-jaccardVerbList = []
+jaccardSubjsList = []
 for sentence in docArticle.sents:
     # computes jaccard score for word lemmas
-    lemmaSent, artWords, artAdjs, artVerbs = sentenceToPOS(sentence)
+    artLemma, artWords, artSubjs = sentenceToPOS(sentence)
     jaccardScore = getJaccardSim(statementWords, artWords)
     jaccardScoreList += [jaccardScore]
     # computes jaccard score for word lemmas with no stop words
     articleNoStops = removeStopWords(artWords)
     jaccardNoStops = getJaccardSim(statementNoStops, articleNoStops)
     jaccardNoStopsList += [jaccardNoStops]
-    # computes jaccard score for adjectives
-    jaccardAdj = getJaccardSim(statementAdjs, artAdjs)
-    jaccardAdjList += [jaccardAdj]
-    # computes jaccard score for verbs
-    jaccardVerb = getJaccardSim(statementVerbs, artVerbs)
-    jaccardVerbList += [jaccardVerb]
+    # computes jaccard score for subject
+    jaccardSubjs = getJaccardSim(statementSubjs, artWords)
+    jaccardSubjsList += [jaccardSubjs]
 
+    # print(artLemma, "*** ", artSubjs, " ***\n")
+    
 
 threeMostSimilarSents = sorted(zip(jaccardScoreList, list(docArticle.sents)), reverse=True)[:3]
 threeMostSimilarNoStops = sorted(zip(jaccardNoStopsList, list(docArticle.sents)), reverse=True)[:3]
-threeMostSimilarAdjs = sorted(zip(jaccardAdjList, list(docArticle.sents)), reverse=True)[:3]
-threeMostSimilarVerbs = sorted(zip(jaccardVerbList, list(docArticle.sents)), reverse=True)[:3]
+threeMostSimilarSubjs = sorted(zip(jaccardSubjsList, list(docArticle.sents)), reverse=True)[:3]
+
 
 print("straight:\n", threeMostSimilarSents, "\n")
 print("No Stops:\n", threeMostSimilarNoStops, "\n")
-print("Adjs:\n", threeMostSimilarAdjs, "\n")
-print("Verbs:\n", threeMostSimilarVerbs, "\n")
+print("Subject:\n", threeMostSimilarSubjs, "\n")
+
 
 
 
